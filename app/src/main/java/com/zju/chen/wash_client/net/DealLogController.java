ce663eae3b31449884274ba7ca0c30ae08edc246
Cyif <chenyifan3x@gmail.com>
2016-07-10 16:22:10 +0800

    ...

---
+++ app/src/main/java/com/zju/chen/wash_client/net/DealLogController.java
@@ -0,0 +1,99 @@
+package com.zju.chen.wash_client.net;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.zju.chen.wash_client.model.DealLog;
+import com.zju.chen.wash_client.model.Room;
+import com.zju.chen.wash_client.util.JacksonUtil;
+import com.zju.chen.wash_client.util.RequestManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chen on 16/7/10.
+ */
+public class DealLogController {
+
+    private String url;
+    private String params;
+    private String httpUrl;
+
+    public List<DealLog> dealLogList = new ArrayList<DealLog>();
+
+    public void getDealLog(final Handler handler, String tel) {
+        params = "/user/store/record/tel/" + tel;
+        httpUrl = url + params;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(httpUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("result");
+
+                            VolleyLog.d("%s", jsonArray.toString());
+                            dealLogList = JacksonUtil.parseJson(jsonArray.toString(), new TypeReference<List<DealLog>>() {
+                            });
+
+                            Message msg = new Message();
+                            msg.what = 1;
+                            handler.sendMessage(msg);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.getStackTrace();
+                    }
+                });
+
+        RequestManager.getInstance().getRequestQueue().add(jsonObjectRequest);
+    }
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getHttpUrl() {
+        return httpUrl;
+    }
+
+    public void setHttpUrl(String httpUrl) {
+        this.httpUrl = httpUrl;
+    }
+
+    public List<DealLog> getDealLogList() {
+        return dealLogList;
+    }
+
+    public void setDealLogList(List<DealLog> dealLogList) {
+        this.dealLogList = dealLogList;
+    }
+}
--- app/src/main/java/com/zju/chen/wash_client/util/CustomApplication.java
+++ app/src/main/java/com/zju/chen/wash_client/util/CustomApplication.java
@@ -11,7 +11,13 @@ public class CustomApplication extends Application{
 
     private static final String URL = "http://115.28.61.157:5000";
 
+    private static final String URL2 = "http://115.28.61.157:8080";
+
+    private static final String TESTTEL = "18868130765";
+
     private String url;
+    private String url2;
+    private String tel;
 
     @Override
     public void onCreate() {
@@ -20,6 +26,8 @@ public class CustomApplication extends Application{
         Log.d("!!!!!!!", "--------------------Application--------------------");
         RequestManager.createInstance(getApplicationContext());
         setUrl(URL);
+        setUrl2(URL2);
+        setTel(TESTTEL);
     }
 
 
@@ -31,4 +39,19 @@ public class CustomApplication extends Application{
         this.url = url;
     }
 
+    public String getUrl2() {
+        return url2;
+    }
+
+    public void setUrl2(String url2) {
+        this.url2 = url2;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 }
